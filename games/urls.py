5858596e2c0c71bb41e0f6ce5a5d99c88676a845
Vlad Zaharia <vladzaharia@gmail.com>
2013-09-18 12:34:33 -0700

    Wildcard host

--- games/urls.py
+++ games/urls.py
@@ -9,11 +9,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'games.views.home', name='home'),
-    url(r'^games/', include('gamesapp.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include('gamesapp.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
 )
